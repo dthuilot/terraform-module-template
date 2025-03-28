53d649d0904468228bc71d99712efa3a7d465372
David Thuilot <dthuilot@hotmail.com>
2025-03-27 22:26:04 -0500

    Add versions.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0.0"
-    }
-    github = {
-      source  = "integrations/github"
-      version = ">= 5.0.0"
-    }
-  }
-}
-
 # Example resource - replace with your actual resources
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = ">= 5.0.0"
+    }
+  }
+}
